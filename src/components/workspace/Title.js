eda1b90e95e07221b856ce54934fd5888292c1f6
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-10-30 13:52:13 +0900

    ✨ add changing spaces function w/ title select

--- src/components/workspace/Title.js
+++ src/components/workspace/Title.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-let Title = function(props) {
-  return (
-    <div>
-      Workspace:{" "}
-      <select>
-        {props.spaces.map(space => (
-          <option>{space.name}</option>
-        ))}
-      </select>
-    </div>
-  );
-};
+class Title extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { shouldRedirect: false };
+    this.onSpaceChange = this.onSpaceChange.bind(this);
+  }
+
+  componentDidUpdate() {
+    // After the space has been changed by the select tag
+    this.setState({ shouldRedirect: false });
+  }
+
+  onSpaceChange(e) {
+    // Don't have to set current b/c Workspace will handle that after redirect
+    this.setState({ shouldRedirect: true });
+  }
+
+  render() {
+    if (this.state.shouldRedirect) {
+      return <Redirect to={`/${this.state.currSpace.name}`} />;
+    }
+    return (
+      <div>
+        Workspace:{" "}
+        <select onChange={this.onSpaceChange}>
+          {this.props.spaces.map(space => (
+            <option value={space.id} selected={space.current}>
+              {space.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   spaces: state.spaces
