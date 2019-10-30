fc24b1f93fa88b600333928e5255d1a7df4abd1b
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-10-30 09:41:12 +0900

    added basic routing setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import "../App.css";
 
 function App() {
-  return <div />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/signup" exact></Route>
+        <Route path="/signin" exact></Route>
+        <Route path="/home" exact></Route>
+        <Route path="/:space_name" exact></Route>
+        <Route>404: No Match</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
