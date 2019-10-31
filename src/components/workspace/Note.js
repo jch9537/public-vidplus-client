9a4d2caa557e28facbe75d257847802c116ab00d
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-11-01 02:38:57 +0900

    ✨ add delete functionality

--- src/components/workspace/Note.js
+++ src/components/workspace/Note.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { changeTimestamp, editNote } from "../../actions/creators";
+import { changeTimestamp, editNote, deleteNote } from "../../actions/creators";
 import { connect } from "react-redux";
 import { Input, Button, Icon } from "antd";
 const { TextArea } = Input;
@@ -8,10 +8,15 @@ class Note extends Component {
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
   }
 
   onChange(e) {
     this.props.editNote({ ...this.props.note, content: e.target.value });
+  }
+
+  deleteNote() {
+    this.props.deleteNote(this.props.note.id);
   }
 
   render() {
@@ -22,7 +27,7 @@ class Note extends Component {
           {note.timestamp}
         </a>
         <TextArea onChange={this.onChange} value={note.content} autoSize />
-        <Button type="primary" shape="circle" className="delete-note">
+        <Button type="primary" shape="circle" onClick={this.deleteNote}>
           <Icon type="delete" style={{ marginLeft: "7.5px" }}></Icon>
         </Button>
       </div>
@@ -32,6 +37,7 @@ class Note extends Component {
 
 const matchDispatchToProps = dispatch => ({
   changeTimestamp: time => dispatch(changeTimestamp(time)),
+  deleteNote: id => dispatch(deleteNote(id)),
   editNote: note => dispatch(editNote(note))
 });
 Note = connect(
