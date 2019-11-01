4eae5d8a10e2d5b97de75d8911dd02f2dc46a6fe
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-11-02 01:39:33 +0900

    🐛 fix textarea bug (cursor keeps moving to end)

--- src/components/workspace/Note.js
+++ src/components/workspace/Note.js
@@ -9,9 +9,22 @@ class Note extends Component {
     super(props);
     this.onChange = this.onChange.bind(this);
     this.deleteNote = this.deleteNote.bind(this);
+    /* If we want to control the TextArea input w/o making cursor jump to the end
+    on every change, we need to manage the state internally (passing content from
+    redux via props.note.content does not remember cursor position) */
+    this.state = { content: props.note.content };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currSpace !== this.props.currSpace) {
+      /* Update content in note's state whenever the workspace changes:
+      Necessary because we are passing this.state.content to TextArea */
+      this.setState({ content: this.props.note.content });
+    }
   }
 
   onChange(e) {
+    this.setState({ content: e.target.value });
     this.props.editNote({ ...this.props.note, content: e.target.value });
   }
 
@@ -26,7 +39,11 @@ class Note extends Component {
         <a href="#" onClick={() => changeTimestamp(note.timestamp)}>
           {note.timestamp}
         </a>
-        <TextArea onChange={this.onChange} value={note.content} autoSize />
+        <TextArea
+          onChange={this.onChange}
+          value={this.state.content}
+          autoSize
+        />
         <Button type="primary" shape="circle" onClick={this.deleteNote}>
           <Icon type="delete" style={{ marginLeft: "7.5px" }}></Icon>
         </Button>
