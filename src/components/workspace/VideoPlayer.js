6b965eba8f58d013347f38b35043d3f1320b79ad
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-11-02 18:10:40 +0900

    🐛 videoplayer inital api call handling

--- src/components/workspace/VideoPlayer.js
+++ src/components/workspace/VideoPlayer.js
@@ -6,27 +6,30 @@ import ReactPlayer from "react-player";
 class VideoPlayer extends Component {
   constructor(props) {
     super(props);
+    // Get the current space from the store (may not exist before fetch happens)
     const currSpace = this.props.spaces.filter(space => space.current)[0];
     this.state = { currSpace };
     this.onProgress = this.onProgress.bind(this);
-    // Create a reference to the ReactPlayer component (for seekTo)
+    // this.player - Create a reference to the ReactPlayer component (for seekTo)
     this.player = React.createRef();
   }
 
   componentDidUpdate(prevProps, prevState) {
     const currSpace = this.props.spaces.filter(space => space.current)[0];
     if (prevState.currSpace !== currSpace) {
+      // space가 바뀌어서 영상이 바뀌어야 된다면
       this.setState({ currSpace });
     } else if (this.props.timestamp !== null) {
+      // Note 컴포넌트에서 timestamp가 눌려서 영상 위치가 바뀌어야 된다면
       const time = this.props.timestamp
         .split(":")
         .reduce(
           (acc, val, i) =>
             i === 0 ? (acc += parseInt(val) * 60) : (acc += parseInt(val)),
           0
-        );
+        ); // specified timestamp (in seconds)
       this.player.current.seekTo(time); // move to specified timestamp
-      this.props.changeTimestamp(null);
+      this.props.changeTimestamp(null); // change timestamp back to null
     }
   }
 
@@ -38,15 +41,19 @@ class VideoPlayer extends Component {
     // Wrap in div with 16:9 aspect ratio
     return (
       <div style={{ position: "relative", paddingBottom: "56.25%" }}>
-        <ReactPlayer
-          ref={this.player}
-          url={this.state.currSpace.url}
-          onProgress={this.onProgress}
-          width="100%"
-          height="100%"
-          style={{ position: "absolute", top: 0, left: 0 }}
-          playing
-        />
+        {this.state.currSpace && (
+          // async fetch handling: 처음에 currSpace가 없어서 currSpace.url로 렌더링 하면 에러남.
+          <ReactPlayer
+            ref={this.player}
+            url={this.state.currSpace.url}
+            onProgress={this.onProgress}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: 0, left: 0 }}
+            controls
+            playing
+          />
+        )}
       </div>
     );
   }
