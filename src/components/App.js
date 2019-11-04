56896319c38337ada2ff04d4946438fde428b49a
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-11-04 15:13:43 +0900

    ✨ add authentication routing for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -15,38 +15,81 @@ import Signup from "./guest/Signup";
 import Home from "./home/Home";
 import Navigation from "./shared/Navigation";
 import { REMOVE_ERROR } from "../actions/types";
+import api from "../api";
 
-let App = function(props) {
-  return (
-    <Router>
-      {props.errors.forEach((error, index) => {
-        alert(error.message);
-        props.removeError(index);
-      })}
-      <Switch>
-        {/*redirect on base URL or /spaces*/}
-        <Route exact path="/(|spaces)">
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/signup" component={Signup}></Route>
-        <Route path="/signin" component={Signin}></Route>
-        <Route
-          path="/(home|spaces)/"
-          render={props => (
-            <Layout style={{ minHeight: "100vh" }}>
-              <Navigation path={props.location.pathname} />
-              <Switch>
-                <Route path="/home" exact component={Home} />
-                <Route path="/spaces/:spaceName" component={Workspace} />
-              </Switch>
-            </Layout>
-          )}
-        />
-        <Route>404: No Match</Route>
-      </Switch>
-    </Router>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { initialRender: true, authenticated: false };
+    this.changeAuthState = this.changeAuthState.bind(this);
+  }
+
+  changeAuthState(callback) {
+    this.setState({ authenticated: !this.state.authenticated }, callback);
+  }
+
+  componentDidMount() {
+    api("user", "GET")
+      .then(() => {
+        this.setState({ initialRender: false, authenticated: true });
+      })
+      .catch(() => {
+        this.setState({ initialRender: false, authenticated: false });
+      });
+  }
+
+  render() {
+    let renderPage;
+    if (this.state.initialRender) {
+      renderPage = () => null;
+    } else if (!this.state.authenticated) {
+      renderPage = () => <Redirect to="/signin" />;
+    } else {
+      renderPage = props => (
+        <Layout style={{ minHeight: "100vh" }}>
+          <Navigation path={props.location.pathname} />
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            <Route path="/spaces/:spaceName" component={Workspace} />
+          </Switch>
+        </Layout>
+      );
+    }
+
+    return (
+      <Router>
+        {this.props.errors.forEach((error, index) => {
+          alert(error.message);
+          this.props.removeError(index);
+        })}
+        <Switch>
+          {/*redirect on base URL or /spaces*/}
+          <Route exact path="/(|spaces)">
+            <Redirect to="/home" />
+          </Route>
+          <Route
+            path="/signup"
+            render={props => (
+              <Signup {...props} authenticated={this.state.authenticated} />
+            )}
+          />
+          <Route
+            path="/signin"
+            render={props => (
+              <Signin
+                {...props}
+                changeAuthState={this.changeAuthState}
+                authenticated={this.state.authenticated}
+              />
+            )}
+          />
+          <Route path="/(home|spaces)/" render={renderPage} />
+          <Route>404: No Match</Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   errors: state.errors
